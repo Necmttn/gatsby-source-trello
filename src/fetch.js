bf60fe6e62e6b4a6102ea7e19582ef844186d81a
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-10-16 22:41:07 +0800

    test data added

--- src/fetch.js
+++ src/fetch.js
@@ -1,35 +1,24 @@
 const request = require('request-promise-native')
 
-
-
-
-
-
-
-const TrelloApi = {
-  boards: (id) => `${TrelloBaseURL}/boards/${id}?fields=id,name&lists=open&list_fields=id,name&key=${API_KEY}&token=${SECRET}`,
-}
-
-
 class TrelloSource {
   constructor(key, secret) {
     if (typeof(key) !== 'string') console.error('key should be string')
     if (typeof(secret) !== 'string') console.error('secret should be string')
     this._key = key
     this._secret = secret
-    this.TrelloBaseURL = 'https://api.trello.com/1/'
+    this.TrelloBaseURL = 'https://api.trello.com/1'
   }
 
-  async getLists(id) {
-    const url = `${this.TrelloBaseURL}boards/${id}?fields=id,name&lists=open&list_fields=id,name&key=${this._key}&token=${this._secret}`
-    const data = await request.get(url)
-    return url
+  async getBoards(id) {
+    const url = `${this.TrelloBaseURL}/boards/${id}?fields=id,name&lists=open&list_fields=id,name&key=${this._key}&token=${this._secret}`
+    try {
+      const data = await request.get(url)
+      return data
+    } catch (err) {
+      throw err
+    }
   }
 
-  static async getBoards(id) {
-    const res = await request.get(TrelloApi.boards(id))
-    console.log(res)
-  }
 }
 
 
--- src/fetch.test.js
+++ src/fetch.test.js
@@ -1,7 +1,6 @@
 const { TrelloSource } = require('./fetch.js')
 
 
-
 const API_KEY = "9ef9dd82a4cfa9ab9f0987478a2c63a6"
 const SECRET = "0b1d9a7dc1539b1050cfc1d9cf77c6c894e3c4c98666b4a062f9719f4eb397d5"
 
@@ -11,18 +10,17 @@ describe('Genereal Type Checkings', () => {
     expect(typeof(TrelloSource)).toBe('function')
   })
 
-
-  test('initilizing the class type checks', () => {
-    const fetcher = new TrelloSource('asd', '123')
-    console.log(fetcher)
-    expect(fetcher.key).toBe('asd')
-    expect(fetcher.secret).toBe('asd')
-  })
-
   test('return something', () => {
     const fetcher = new TrelloSource(API_KEY, SECRET)
 
-    const list = fetcher.getLists('PP')
+    const list = fetcher.getBoards('PP')
     console.log('list', list)
+  })
+
+  test('return data', async () => {
+    const fetcher = new TrelloSource(API_KEY, SECRET)
+
+    const list = await fetcher.getBoards('u1Rg4E5s')
+    expect(data).toBe('something')
   })
 })
